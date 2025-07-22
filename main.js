8e3d9fccf73b617e73f37622720cbb9adbc6f893
Peter Kohler <github@kohler.world>
2025-07-22 18:56:10 +0200

    Major refactoring to use new R2 Web API

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ const utils = require('@iobroker/adapter-core');
 // Load your modules here, e.g.:
 // const fs = require("fs");
 const axios = require('axios');
+// Add cookie jar support
+// @ts-ignore: No type declarations for axios-cookiejar-support in JS
+const { wrapper } = require('axios-cookiejar-support');
+const { CookieJar } = require('tough-cookie');
+// Patch axios for cookie jar support (for CommonJS)
+// wrapper(axios); // This line is removed as per the edit hint
 
 /**
  * The adapter instance
@@ -20,10 +26,19 @@ const axios = require('axios');
 // current poll period: 5min (300000ms)
 const UPDATE_PERIOD=300000;
 let adapter;
-let authToken='';
+// Remove authToken
 let gw='';
-let currentComfortTemp=0;
 let boilerInterval;
+let isLoggedIn = false;
+let lastPlantData = null;
+
+// Create a cookie jar and axios instance for session persistence
+const cookieJar = new CookieJar();
+// @ts-ignore: 'jar' is a runtime property supported by axios-cookiejar-support
+const axiosInstance = wrapper(axios.create({
+    jar: cookieJar,
+    withCredentials: true
+}));
 
 // Update state variable
 function updateState(StateID, value) {
@@ -38,10 +53,9 @@ function updateState(StateID, value) {
 
 // Main loop, fetch data from boiler
 function updateBoiler() {
-
     adapter.log.debug('updateBoiler()');
 
-    if (authToken=='') {
+    if (!isLoggedIn) {
         adapter.log.debug('Not logged in yet, attempting login');
         loginBoiler();
         return;
@@ -51,206 +65,171 @@ function updateBoiler() {
         return;
     }
 
-    const url='https://www.ariston-net.remotethermo.com/api/v2/velis/slpPlantData/' +
-        gw + '?umSys=si';
+    // New API URL
+    const url = `https://www.ariston-net.remotethermo.com/R2/PlantHomeSlp/GetData/${gw}?fetchSettings=true&fetchTimeProg=false`;
 
-    axios.get(url, {
-        headers: {
-            'Ar.authtoken': authToken
-        }
-    }).then(function (response) {
-        if (response.status==200) {
-            adapter.log.debug('Got waterTemp from ariston-net: ' + response.data.waterTemp);
-            // update state with values received from API
-            updateState('ariston-remotethermo.0.boiler.waterTemp', response.data.waterTemp);
-            updateState('ariston-remotethermo.0.boiler.comfortTemp', response.data.comfortTemp);
-            updateState('ariston-remotethermo.0.boiler.mode', response.data.mode);
-            updateState('ariston-remotethermo.0.boiler.opMode', response.data.opMode);
-            updateState('ariston-remotethermo.0.boiler.boostOn', response.data.boostOn);
-            updateState('ariston-remotethermo.0.boiler.hpState', response.data.hpState);
-            updateState('ariston-remotethermo.0.boiler.on', response.data.on);
-        } else if (response.status==405) {
-            adapter.log.info('updateBoiler(): Authentication expired, re-login');
-            loginBoiler();
-        } else {
-            adapter.log.error('updateBoiler(): Unexpected HTTP response code: ' + response.status);
-        }
-    })
+    axiosInstance.get(url)
+        .then(function (response) {
+            if (response.data && response.data.ok && response.data.data && response.data.data.plantData) {
+                const plantData = response.data.data.plantData;
+                adapter.log.debug('Got plantData from ariston-net: ' + JSON.stringify(plantData));
+                // update state with values received from API
+                updateState('ariston-remotethermo.0.boiler.waterTemp', plantData.waterTemp);
+                updateState('ariston-remotethermo.0.boiler.comfortTemp', plantData.comfortTemp);
+                updateState('ariston-remotethermo.0.boiler.mode', plantData.mode);
+                updateState('ariston-remotethermo.0.boiler.opMode', plantData.opMode);
+                updateState('ariston-remotethermo.0.boiler.boostOn', plantData.boostOn);
+                updateState('ariston-remotethermo.0.boiler.hpState', plantData.hpState);
+                updateState('ariston-remotethermo.0.boiler.on', plantData.on);
+                // Store latest plantData for use in setBoiler
+                lastPlantData = plantData;
+            } else {
+                adapter.log.error('updateBoiler(): Unexpected response: ' + JSON.stringify(response.data));
+            }
+        })
         .catch(function (error) {
-            if (error.response.status==429) {
+            if (error.response && error.response.status==429) {
                 const secs=error.response.data.match(/\d+/)[0];
                 adapter.log.info(`updateBoiler(): Got throttled ${secs} seconds`);
                 adapter.log.debug(`updateBoiler(): Rescheduling updateBoiler ${secs} seconds`);
                 setTimeout(() => { updateBoiler(); },(parseInt(secs)+1)*1000);
-            } else if (error.response.status==405) {
-                adapter.log.info('updateBoiler(): Authentication expired, re-login 2');
-                loginBoiler();
             } else {
-                adapter.log.error('updateBoiler(): Unexpected HTTP response code: ' + error.response.status);
+                adapter.log.error('updateBoiler(): HTTP error: ' + (error.response ? error.response.status : error.message));
             }
         });
 }
 
-function getGateway() {
+// Generic function to set boiler parameters
+function setBoiler(viewModelChanges, callback) {
+    if (!lastPlantData || !gw) {
+        adapter.log.error('setBoiler(): No plant data or gateway ID available.');
+        if (callback) callback(false);
+        return;
+    }
+    const url = `https://www.ariston-net.remotethermo.com/R2/PlantHomeSlp/SetData/${gw}`;
+    // Compose the full viewModel from lastPlantData
+    const fullViewModel = {
+        on: lastPlantData.on,
+        plantMode: lastPlantData.mode, // map mode to plantMode
+        opMode: lastPlantData.opMode,
+        boostOn: lastPlantData.boostOn,
+        comfortTemp: lastPlantData.comfortTemp,
+        holidayUntil: lastPlantData.holidayUntil
+        // Add more fields as needed
+    };
+    // Apply changes
+    Object.assign(fullViewModel, viewModelChanges);
+    // Compose the payload
+    const payload = {
+        plantData: Object.assign({}, lastPlantData, { gatewayId: gw }),
+        viewModel: fullViewModel
+    };
+    adapter.log.debug('setBoiler(): Sending payload: ' + JSON.stringify(payload));
+    axiosInstance.post(url, payload)
+        .then(function (response) {
+            if (response.data && response.data.ok) {
+                adapter.log.info('setBoiler(): Update successful');
+                if (callback) callback(true);
+            } else {
+                adapter.log.error('setBoiler(): Update failed: ' + JSON.stringify(response.data));
+                if (callback) callback(false);
+            }
+        })
+        .catch(function (error) {
+            adapter.log.error('setBoiler(): HTTP error: ' + (error.response ? error.response.status : error.message));
+            if (callback) callback(false);
+        });
+}
 
-    adapter.log.debug('getGateway()');
-
-    axios.get('https://www.ariston-net.remotethermo.com/api/v2/velis/plants', {
-        headers: {
-            'Ar.authtoken': authToken
-        }
-    }).then(function (response) {
-        if (response.status==200) {
-            gw=response.data[0].gw;
-            adapter.log.info(`Got GatewayID: ${gw}`);
-            adapter.setStateAsync('ariston-remotethermo.0.boiler.gw',
-                {val: response.data[0].gw, ack: true});
-            // everything's ready now, fetch status from API
-            updateBoiler();
-        } else {
-            adapter.log.error('getGateway(): Unexpected HTTP return code ' + response.status);
-        }
-    }).catch(function (error) {
-        if (error.response.status==429) {
-            const secs=error.response.data.match(/\d+/)[0];
-            adapter.log.info(`getGateway(): Got throttled ${secs} seconds`);
-            adapter.log.debug(`Rescheduling getGateway ${secs} seconds`);
-            setTimeout(() => { getGateway(); },(parseInt(secs)+1)*1000);
-        } else {
-            adapter.log.error('getGateway(): Unexpected HTTP return code ' + error.response.status);
+// Refactor updateComfortTemp to use setBoiler and update state on success
+function updateComfortTemp(newTemp) {
+    if (!gw) return;
+    setBoiler({ comfortTemp: newTemp }, function(success) {
+        if (success) {
+            adapter.setStateAsync('ariston-remotethermo.0.boiler.comfortTemp', { val: newTemp, ack: true });
         }
     });
 }
 
-function updateComfortTemp(newTemp) {
-
-    if (gw=='') return;
-
-    const update={
-        'new': {
-            'comfort': newTemp,
-            'reduced': 0.0
-        },
-        'old': {
-            'comfort': currentComfortTemp,
-            'reduced': 0.0
-        }
-    };
-
-    const url='https://www.ariston-net.remotethermo.com/api/v2/velis/slpPlantData/' +
-        gw + '/temperatures?umSys=si';
-    axios.post(url, update, {headers: {'Ar.authtoken': authToken}})
-        .then(function (response) {
-            if (response.status==200) {
-                adapter.log.info('Update comfortTemp successful');
-                adapter.setStateAsync('ariston-remotethermo.0.boiler.comfortTemp',
-                    {ack: true});
-                currentComfortTemp=newTemp;
-            }
-        })
-        .catch(function (error) {
-            if (error.response.status==429) {
-                const secs=error.response.data.match(/\d+/)[0];
-                adapter.log.info(`updateComfortTemp(): Got throttled ${secs} seconds`);
-                adapter.log.debug(`updateComfortTemp(): Rescheduling ${secs} seconds`);
-                setTimeout(() => { updateComfortTemp(newTemp);},(parseInt(secs)+1) * 1000);
-            } else {
-                adapter.log.error('updateComfortTemp(): Unexpected HTTP return code ' + error.response.status);
-            }
-        });
-}
-
+// Refactor onOff to use setBoiler and update state on success
 function onOff(target) {
-
-    if (gw=='') return;
-
+    if (!gw) return;
     adapter.log.debug('OnOff: ' + target.toString());
-
-    const url='https://www.ariston-net.remotethermo.com/api/v2/velis/slpPlantData/' +
-    gw + '/switch';
-    axios.post(url, target.toString(), {headers: {'Ar.authtoken': authToken, 'Content-Type': 'application/json'}})
-        .then(function (response) {
-            if (response.status==200) {
-                if (target) {
-                    adapter.log.info('Boiler switched on');
-                } else {
-                    adapter.log.info('Boiler switched off');
-                    adapter.setStateAsync('ariston-remotethermo.0.boiler.on',
-                        {ack: true});
-                }
-            }
-        })
-        .catch(function (error) {
-            if (error.response.status==429) {
-                const secs=error.response.data.match(/\d+/)[0];
-                adapter.log.info(`onOff(): Got throttled ${secs} seconds`);
-                adapter.log.debug(`onOff(): Rescheduling ${secs} seconds`);
-                setTimeout(() => { onOff(target); },(parseInt(secs)+1) * 1000);
-            } else {
-                adapter.log.error('onOff(): Unexpected HTTP return code ' + error.response.status);
-            }
-        });
+    setBoiler({ on: !!target }, function(success) {
+        if (success) {
+            adapter.setStateAsync('ariston-remotethermo.0.boiler.on', { val: !!target, ack: true });
+        }
+    });
 }
 
+// Refactor boost to use setBoiler and update state on success
 function boost(target) {
-    if (gw=='') return;
+    if (!gw) return;
+    setBoiler({ boostOn: !!target }, function(success) {
+        if (success) {
+            adapter.setStateAsync('ariston-remotethermo.0.boiler.boostOn', { val: !!target, ack: true });
+        }
+    });
+}
 
-    let state='false';
-    if (target==true) state='true';
+function getGateway() {
+    adapter.log.debug('getGateway()');
 
-    const url='https://www.ariston-net.remotethermo.com/api/v2/velis/slpPlantData/' +
-        gw + '/boost';
-    axios.post(url, state, {headers: {'Ar.authtoken': authToken}})
-        .then(function (response) {
-            if (response.status==200) {
-                if (target)
-                    adapter.log.info('Boiler boost on');
-                else
-                    adapter.log.info('Boiler boost off');
-                adapter.setStateAsync('ariston-remotethermo.0.boiler.boostOn',
-                    {ack: true});
+    // Step 1: GET request to root
+    axiosInstance.get('https://www.ariston-net.remotethermo.com/')
+        .then(async function () {
+            // Step 2: Inspect cookies for gateway ID
+            const cookies = await cookieJar.getCookies('https://www.ariston-net.remotethermo.com/');
+            const gwCookie = cookies.find(c => c.key.startsWith('ar.externalDomoticSystem_'));
+            if (gwCookie) {
+                // Step 3: Extract gateway ID from key
+                const parts = gwCookie.key.split('_');
+                if (parts.length === 2) {
+                    gw = parts[1];
+                    adapter.log.info(`Got GatewayID from cookie: ${gw}`);
+                    adapter.setStateAsync('ariston-remotethermo.0.boiler.gw', {val: gw, ack: true});
+                    // Step 4: Fetch status from API
+                    updateBoiler();
+                } else {
+                    adapter.log.error('getGateway(): Unexpected cookie key format: ' + gwCookie.key);
+                }
+            } else {
+                adapter.log.error('getGateway(): Gateway cookie not found.');
             }
         })
         .catch(function (error) {
-            if (error.response.status==429) {
-                const secs=error.response.data.match(/\d+/)[0];
-                adapter.log.info(`boost(): Got throttled ${secs} seconds`);
-                adapter.log.debug(`boost(): Rescheduling ${secs} seconds`);
-                setTimeout(() => {boost(target);},(parseInt(secs)+1) * 1000);
-            } else {
-                adapter.log.error('boost(): Unexpected HTTP return code ' + error.response.status);
-            }
+            adapter.log.error('getGateway(): HTTP error: ' + (error.response ? error.response.status : error.message));
         });
 }
 
 function loginBoiler() {
-
     if (adapter.config.password=='' || adapter.config.email=='') {
         adapter.log.error('Login Data missing...');
+        isLoggedIn = false;
         return;
     }
 
-    axios.post('https://www.ariston-net.remotethermo.com/api/v2/accounts/login', {
-        usr: adapter.config.email,
-        pwd: adapter.config.password,
-        imp: false,
-        notTrack: true,
-        appInfo: {
-            os: 2,
-            appVer: '5.6.772.40151',
-            appId: 'com.remotethermo.aristonnet'
-        }
+    // New login API
+    axiosInstance.post('https://www.ariston-net.remotethermo.com/R2/Account/Login', {
+        email: adapter.config.email,
+        password: adapter.config.password,
+        rememberMe: true,
+        language: 'English_Us'
     })
         .then(function (response) {
-            if (response.status==200 && 'token' in response.data) {
-                adapter.log.info('Login Successful, got token');
-                authToken=response.data.token;
-                // now fetch gateway ID
+            if (response.data && response.data.ok === true) {
+                adapter.log.info('Login successful (cookie-based session)');
+                isLoggedIn = true;
+                // Now fetch gateway ID
                 getGateway();
+            } else {
+                adapter.log.error('Login failed: ' + JSON.stringify(response.data));
+                isLoggedIn = false;
             }
         })
         .catch(function (error) {
-            adapter.log.error('boost(): Unexpected HTTP return code ' + error.response.status);
+            adapter.log.error('loginBoiler(): HTTP error: ' + (error.response ? error.response.status : error.message));
+            isLoggedIn = false;
         });
 }
 
